ca9e19273c7d8728242b52954b37423061d203a1
Araki-SH <araki.shunsuke.ap9@gmail.com>
2023-07-05 20:54:49 +0900

    change directory

--- Kadai1MLP/docker/run_docker.sh
+++ Kadai1MLP/docker/run_docker.sh
@@ -2,11 +2,11 @@
 
 PARENT_DIR=$(pwd|xargs dirname)
 
-docker run --name kadai1_mlp -it --rm -d --gpus all --ipc=host \
+docker run --name kadai1mlp -it --rm -d --gpus all --ipc=host \
            --ulimit memlock=-1 --ulimit stack=67108864 \
            -v /tmp/.X11-unix:/tmp/.X11-unix: -v "${PARENT_DIR}":/home/"${USER}"/python-project-rllab-kadai1/Kadai1MLP \
            -e DISPLAY="${DISPLAY}" \
            -p 63322:63322 \
            -p 6006:6006 \
            -p 6007:6007 \
-           kadai1_mlp jupyter lab --config='./Kadai1MLP/jupyter_lab_config.py'
+           kadai1mlp jupyter lab --config='./Kadai1MLP/jupyter_lab_config.py'
